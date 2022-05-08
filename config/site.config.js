122ce773ae0d493297ba13cc7cd1183473a275ca
chzhxun <102458259+chzhxun@users.noreply.github.com>
2022-05-08 21:45:38 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "Xun's OneDrive",
+  title: "Xun's Image-hosting-web-site",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
@@ -50,11 +50,11 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      link: 'https://github.com/chzhxun/onedrive-vercel-index',
     },
     {
       name: 'Telegram',
-      link: 'https://t.me/realSpencerWoo',
+      link: '',
     },
   ],
 
