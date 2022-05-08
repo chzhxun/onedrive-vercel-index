b7c70846cfd9e5a3780c268a3df5e8d4ca86ad98
chzhxun <102458259+chzhxun@users.noreply.github.com>
2022-05-08 09:43:14 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   title: "Xun's OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Public',
+  baseDirectory: '/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -42,7 +42,7 @@ module.exports = {
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: chxunice@qq.com',
+  email: 'mailto:chxunice@qq.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
@@ -50,11 +50,11 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/chzhxun/onedrive-vercel-index',
+      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
     },
     {
       name: 'Telegram',
-      link: '',
+      link: 'https://t.me/realSpencerWoo',
     },
   ],
 
